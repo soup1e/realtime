f9bf542b99a8e97c95d4339acdc0b81ee476b65e
Tom Booth <tombeezle@gmail.com>
2022-10-11 12:57:37 -0700

    JS on add post functionality

--- add-post/add-post.js
+++ add-post/add-post.js
@@ -4,9 +4,21 @@ import { addPost } from '../fetch-utils.js';
 
 //DOM Elements
 const postForm = document.getElementById('post-form');
+const errorDisplay = document.getElementById('error-display');
 
 //State
+let error = null;
 
 //Events
+postForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(postForm);
+
+    const post = {
+        title: formData.get('title'),
+        text: formData.get('text'),
+    };
+});
 
 //Display-Functions
