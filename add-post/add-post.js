618b08c6a86a93089bf8f8fee813a4e07ff5adf1
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-10-11 12:43:53 -0700

    Starting of JS

---
+++ add-post/add-post.js
@@ -0,0 +1,12 @@
+//Imports
+import '../auth/user.js';
+import { addPost } from '../fetch-utils.js';
+
+//DOM Elements
+const postForm = document.getElementById('post-form');
+
+//State
+
+//Events
+
+//Display-Functions
--- fetch-utils.js
+++ fetch-utils.js
@@ -28,3 +28,7 @@ export async function signOutUser() {
 }
 
 /* Data functions */
+
+export async function addPost(post) {
+    return await client.from('posts').insert(post).single();
+}
